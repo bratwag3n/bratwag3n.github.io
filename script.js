99a0fab365a81555732ea23750ee48b32ef593be
Kaitlyn Frye <fryek2845@gmail.com>
2026-05-26 13:20:11 -0400

    csscompleteoverhaulpt1

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// Resize iframe to fit content
-function resizeIframe() {
-    const iframe = document.getElementById('atabook-iframe');
-    if (iframe) {
-        try {
-            // Try to get content height
-            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-            const height = iframeDoc.documentElement.scrollHeight || iframeDoc.body.scrollHeight;
-            iframe.style.height = height + 'px';
-        } catch (e) {
-            // Cross-origin restriction, set a reasonable height
-            console.log('Cross-origin restriction, using fallback height');
-            iframe.style.height = '800px';
-        }
-    }
-}
-
-// Resize on load and periodically check for content changes
-window.addEventListener('load', function() {
-    const iframe = document.getElementById('atabook-iframe');
-    if (iframe) {
-        iframe.addEventListener('load', resizeIframe);
-        // Also check periodically in case content loads dynamically
-        setInterval(resizeIframe, 1000);
-    }
-});
 
 let slideIndex = 1;
 showSlides(slideIndex);
